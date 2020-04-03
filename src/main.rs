15d3de5c4fd517b63ce711f3b0b25fb85e30fea4
John <johnmsaylor@gmail.com>
2020-04-03 14:23:21 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let string = String::from("Peep");
+
+    print!("{}", first_word(&string));
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
